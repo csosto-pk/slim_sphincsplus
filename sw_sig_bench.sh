a6a333ea907746a269b70a64b5bc83ba08c59554
csosto-pk <pankab@gmail.com>
2019-09-30 15:52:01 -0400

    Print the stack in benchmark script

--- sw_sig_bench.sh
+++ sw_sig_bench.sh
@@ -3,76 +3,87 @@
 # Run as ./sw_sig_bench.sh > sw_sig_bench.results 
 
 echo ------------------
-cd ref 
-rm params.h 
+cd ref && rm params.h 
 ln -s params/params-sphincs-sha256-192-h15-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-192-h15-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-192-h20-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-192-h20-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-192-h35-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h 
 ln -s params/params-sphincs-sha256-192-h35-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h15-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h15-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h20-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h20-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h35-w16.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
 cd ../ref && rm params.h  
 ln -s params/params-sphincs-sha256-256-h35-w256.h params.h 
-make clean && make benchmark | egrep "(Parameters|Generating|Signing)"
+make clean && make benchmark | egrep "(Parameters|Generating|Signing)" 
+make sig-ver | grep "Stack used"
 cd ../sha256-avx2/ && make clean && make benchmark | egrep "(Verifying|size)"
 echo ------------------
 
